d95e120881c44bcf97cff0ae869e10a76168ab11
Eugene Fedin <eugfed@gmail.com>
2013-01-18 15:33:03 +0400

    cloudmade isn’t used

--- iks.js
+++ iks.js
@@ -17,12 +17,12 @@ var ikData = [
 
 var getMapYaCentered = function() {
 	var map = new L.Map('map'),
-		cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/4dbea02acb2d47779913c727fa16dda9/997/256/{z}/{x}/{y}.png',
+		/*cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/4dbea02acb2d47779913c727fa16dda9/997/256/{z}/{x}/{y}.png',
 			{
 				attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 				maxZoom: 18
 			}
-		),
+		),*/
 		mapnik = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			{
 				attribution: 'Map data &copy; <a href="http://osm.org">OpenStreetMap</a> contributors',
